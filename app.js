7c205e426438b7c1f2934470f41d78fe6388574d
oyinade olawoyin <oyinadeolawoyin@proton.me>
2025-06-05 05:51:42 +0100

    Add origin https to cors

--- app.js
+++ app.js
@@ -11,9 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: ["http://localhost:5173/"],
+    origin: ["http://localhost:5173"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true,
 }));
 
 app.use("/api/user", userRoutes);
